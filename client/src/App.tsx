de4cd303819255a616c74de38a7cac1fbb575809
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-04-20 23:13:00 +0530

    add more page and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,17 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 
 function App() {
 
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      try {
+        const res = await axios.get("http://localhost:8001/user/me");
+        return res.data.user;
+      } catch (e) {
+        return null
+      }
+    },
+    retry:false,
+  })
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading....
+      </div>
+    )
+  }
 
 
   return (
     
     <BrowserRouter>
+      {authuser && <SideBar/>}
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={authUser ? <Dashboard /> :<Home/>} />
+        <Route path="/login" element={!authUser ? <Login /> : <Dashboard />} />
+        <Route path='/signup' element={!authUser ? <Signup /> : <Dashboard />} />
+        <Route path='/dashboard' element={!authUser ? <Dashboard /> : <Home />} />
         
     </Routes>
     </BrowserRouter>
