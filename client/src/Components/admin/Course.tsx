88a28e9412c6c9185df38255ba43187577fcb7a1
yash gupta <141262414+yashtech00@users.noreply.github.com>
2025-04-23 16:54:32 +0530

    update add , edit model function

--- client/src/Components/admin/Course.tsx
+++ client/src/Components/admin/Course.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { courseProp } from "../DashboardPage"
 import axios from "axios";
 import { CourseCard } from "./CourseCard";
+import { Navbar } from "../Navbar";
 
 export const Course = () => {
 
@@ -10,12 +11,11 @@ export const Course = () => {
     useEffect(() => {
         const handleFetch = async () => {
             try {
-                const res = await axios.get("http://localhoast:8001/admin/AllCourse", { withCredentials: true });
+                const res = await axios.get("http://localhost:8001/admin/AllCourse", { withCredentials: true });
                 console.log(res,"admin course");
                 setCourse(res.data.data);
             } catch (e:any) {
                 console.error(e.message);
-                
             }
         }
         handleFetch();
@@ -24,15 +24,25 @@ export const Course = () => {
 
 
     return (
-        <div>
-            hello
-            <div>
+        <div className="">
+            <Navbar />
+            <div className="m-10">
+            <div className="flex justify-between">
+                <div className="font-bold text-xl">Admin Panel</div>
+                <div>
+                    <button className="border-2 border-stone-900 px-4 py-2 hover:bg-stone-900 ">Add Course +</button>
+                </div>
+                </div>
+                <div className="m-2">
+            <div className="grid grid-cols-3 mt-6 mx-4 ">
                 {course.map((AllCourse) => (
                     <div key={AllCourse._id}>
                         <CourseCard course={ AllCourse} />
                     </div>
                 ))}
+                    </div>
+                    </div>
             </div>
-        </div>
+            </div>
     )
 }
